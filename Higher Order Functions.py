a0018cef672e67bc9c1c05ebe040c3db8b0f340b
Magnus Herweyer <magnus.herweyer@gmail.com>
2022-08-06 13:22:55 -0500

    Adds another example with curse words

--- Higher Order Functions.py
+++ Higher Order Functions.py
@@ -11,17 +11,36 @@ def divide (x,y):
 
 # In this function I want to check that y does not equal 0, and then call that helped function
 
-def check_if_y_zero(func):
-    def safe_version(*args):
+# def check_if_y_zero(func):
+#     def safe_version(*args):
+#         if args[1] == 0:
+#             print("Y is 0, cannot divide by 0")
+#             return
+#         return func(*args)
+#     return safe_version
+#
+# divide_safely = check_if_y_zero(divide)
+# print(divide_safely(5,0))
+
+
+#duplicatively work here - I'm doing the same function again but down here for practice
+
+#GOAL: to call a divide() fucntion inside of a divbide by 0 check function
+
+
+def check_for_zero(func):
+    #checking ALL args for the
+    def number_condom(*args):
         if args[1] == 0:
-            print("Y is 0, cannot divide by 0")
+            print("What the fuck do you think you're doing bringing that weak shit in here? You know you can't dividie by 0 baby!")
             return
         return func(*args)
-    return safe_version
+    return number_condom
 
-divide_safely = check_if_y_zero(divide)
-print(divide_safely(5,0))
+std_free_division = check_for_zero(divide)
 
+print(std_free_division(5,30))
+print(std_free_division(5,0))
 
 
 
