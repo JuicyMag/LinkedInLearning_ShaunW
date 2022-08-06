bac87b6314b46cef79cb1d9d915f0a46f92947dc
Magnus Herweyer <magnus.herweyer@gmail.com>
2022-08-06 13:54:39 -0500

    adds list substract function

--- Map, Filter, List.py
+++ Map, Filter, List.py
@@ -11,3 +11,16 @@ for num in numbers_list:
 print(double_list)
 
 #above is the old way. Below is using map()
+
+#first define the basic function you want, in this case doubling, but could also be diviing, tripling, etc.
+
+def double(x):
+    return x*2
+
+def subtract_from_list(x):
+    return x-1
+
+double_list_fucntion = list(map(double, numbers_list))
+subtract_from_list = list(map(subtract_from_list, numbers_list))
+print(double_list_fucntion)
+print(subtract_from_list)
